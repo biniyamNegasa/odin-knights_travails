ea937e5f65126af8aae76f74a31473b223058c39
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-10-02 09:42:11 +0300

    Edit README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-knights_travails
+
+An implementation of a function that shows the path it takes for a knight(chess) to go from one vertex to another.
+
--- main.rb
+++ main.rb
@@ -48,8 +48,8 @@ def knight_moves(first_cor, second_cor)
   end
 end
 
-knight_moves([0, 0], [3, 3])
-knight_moves([0, 0], [7, 7])
-knight_moves([0, 0], [1, 2])
-knight_moves([3, 3], [0, 0])
-knight_moves([3, 3], [4, 3])
+# knight_moves([0, 0], [3, 3])
+# knight_moves([0, 0], [7, 7])
+# knight_moves([0, 0], [1, 2])
+# knight_moves([3, 3], [0, 0])
+# knight_moves([3, 3], [4, 3])
